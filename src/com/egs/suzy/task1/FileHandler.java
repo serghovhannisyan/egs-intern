ed31ab6887dc80f9cb8e5f275bdb199be5ee4fb6
Suzie Melkonyan <melkonyansuzie@gmail.com>
2020-03-24 22:47:45 +0400

    applied changes

--- src/com/egs/suzy/task1/FileHandler.java
+++ src/com/egs/suzy/task1/FileHandler.java
@@ -1,16 +1,18 @@
 import java.io.*;
 
 public class FileHandler {
-    public static void CountHellos(File in, File out){
+    public static void countHellos(File in, File out){
         try(BufferedReader reader = new BufferedReader(new FileReader(in));
         BufferedWriter writer = new BufferedWriter(new FileWriter(out, true))){
             String[] arr;
             String line = reader.readLine();;
             int count = 0, lineNum = 0;
-            while(line!=null){
+            while(true){
               arr = line.split("\\s");
               for(String s : arr){
-                  if(s.equalsIgnoreCase("hello"))  count++;
+                  if(s.equalsIgnoreCase("hello")){
+                      count++;
+                  }
               }
               writer.write(lineNum +"-"+count);
               writer.newLine();
--- src/com/egs/suzy/task1/TestCountHellos.java
+++ src/com/egs/suzy/task1/TestCountHellos.java
@@ -2,14 +2,14 @@ import java.io.File;
 import java.io.IOException;
 
 public class TestCountHellos {
+    private final String INPUT_FILENAME = "hello.txt";
+    private  final String RESULT_FILENAME = "result.txt";
     public static void main(String[] args) throws IOException {
-        File hello = new File("hello.txt");
-        File result = new File("result.txt");
+        File hello = new File(INPUT_FILENAME);
+        File result = new File(RESULT_FILENAME);
         if(result.exists()){
             result.delete();
-            result.createNewFile();
         }
-        hello.createNewFile();
         FileHandler.CountHellos(hello, result);
     }
 
