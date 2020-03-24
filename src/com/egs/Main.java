28077636a8884c935dbe0836ff798eb367440a23
Sergey Hovhannisyan <sergeyh@energizeglobal.com>
2020-03-24 20:31:38 +0400

    Second commit

--- src/com/egs/Main.java
+++ src/com/egs/Main.java
@@ -1,10 +1,10 @@
 package com.egs;
 
-import com.egs.serg.MyApp;
+import com.egs.serg.task1.MyApp1;
 
 public class Main {
 
     public static void main(String[] args) {
-        new MyApp().init();
+        new MyApp1().init();
     }
 }
---
+++ src/com/egs/serg/task1/MyApp1.java
@@ -0,0 +1,8 @@
+package com.egs.serg.task1;
+
+public class MyApp1 {
+
+    public void init() {
+        // starting point
+    }
+}
