62d9f36cde760eff4ef008dfa9bbce34472e604e
NikDan95 <danielyan.nikol@gmail.com>
2020-03-30 14:57:58 +0400

    homeWork/task3

--- src/com/egs/nikol/task1/FileExplorer.java
+++ src/com/egs/nikol/task1/FileExplorer.java
@@ -6,35 +6,28 @@ public class FileExplorer {
 
     public static void fileTransfer(File data, File result) throws IOException {
 
-        BufferedReader reader = new BufferedReader(new FileReader(data));
-        BufferedWriter writer = new BufferedWriter(new FileWriter(result));
+
         String line;
         int count;
         int linesNumber = 1;
-        while (true) {
-            line = reader.readLine();
-            if (line != null) {
 
-                count = 0;
-                try {
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(data)); BufferedWriter writer = new BufferedWriter(new FileWriter(result))) {
+
+            while (true) {
+                line = reader.readLine();
+                if (line != null) {
+                    count = 0;
                     String[] words = line.split(" ");
                     for (String s : words) {
                         if (s.equalsIgnoreCase("hello")) {
                             count++;
                         }
                     }
-                } catch (NullPointerException e) {
-                    e.printStackTrace();
-                }
+                } else break;
                 writer.write("line " + (linesNumber++) + " has " + count + " <<Hello>> word");
                 writer.newLine();
-            } else {
-                reader.close();
-                writer.close();
-                break;
             }
-
-
         }
     }
 }
